ec2ee6028ea6327def2a3846a36015625e7b1629
mariuszglu <pinkiweb88@gmail.com>
2023-02-05 17:27:34 +0100

    create 3 rectangle, example

--- canvas.js
+++ canvas.js
@@ -3,3 +3,7 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
 const c = canvas.getContext('2d')
+c.fillRect(100,100,100,100)
+c.fillRect(200,200,100,100)
+c.fillRect(300,300,100,100)
+
