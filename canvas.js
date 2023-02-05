523a24f93f9dfef7fb90c908a5b71c0ee45d1689
mariuszglu <pinkiweb88@gmail.com>
2023-02-05 22:40:40 +0100

    loop animate circle in x position left-right

--- canvas.js
+++ canvas.js
@@ -3,44 +3,62 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
 const c = canvas.getContext('2d')
-c.fillStyle = 'rgba(255,0,0,0.5)'
-c.fillRect(50, 100, 100, 100)
-c.fillStyle = 'rgba(0,255,0,0.2)'
-c.fillRect(200, 50, 100, 100)
-c.fillStyle = 'rgba(0,0,255,0.5)'
-c.fillRect(300, 150, 100, 100)
+// c.fillStyle = 'rgba(255,0,0,0.5)'
+// c.fillRect(50, 100, 100, 100)
+// c.fillStyle = 'rgba(0,255,0,0.2)'
+// c.fillRect(200, 50, 100, 100)
+// c.fillStyle = 'rgba(0,0,255,0.5)'
+// c.fillRect(300, 150, 100, 100)
 
-//line
+// //line
 
-c.beginPath()
-c.moveTo(100, 300)
-c.lineTo(300, 100)
+// c.beginPath()
+// c.moveTo(100, 300)
+// c.lineTo(300, 100)
 
-c.moveTo(100, 100)
-c.lineTo(300, 100)
+// c.moveTo(100, 100)
+// c.lineTo(300, 100)
 
-c.moveTo(100, 300)
-c.lineTo(300, 300)
+// c.moveTo(100, 300)
+// c.lineTo(300, 300)
 
-c.strokeStyle = 'red'
-c.stroke()
+// c.strokeStyle = 'red'
+// c.stroke()
 
 //arc / circle
 
 // c.arc(x: number, y: number, radius: number, startAngle: number, endAngle: number, counterclockwise?: boolean | undefined): void)
-c.beginPath()
-c.arc(300, 300, 30, 0,Math.PI*2,false)
-c.strokeStyle = 'green'
-c.stroke()
+// c.beginPath()
+// c.arc(300, 300, 30, 0,Math.PI*2,false)
+// c.strokeStyle = 'green'
+// c.stroke()
 
-for (let i = 0; i < 100; i++) {
-    
-    const x = Math.random()*window.innerWidth
-    const y = Math.random()*window.innerHeight
+// for (let i = 0; i < 100; i++) {
 
-    c.beginPath()
-    c.strokeStyle = 'black'
-    c.arc(x,y,30,0,Math.PI*2,false)
-    c.stroke()
-    
+//     const x = Math.random()*window.innerWidth
+//     const y = Math.random()*window.innerHeight
+
+//     c.beginPath()
+//     c.strokeStyle = 'black'
+//     c.arc(x,y,30,0,Math.PI*2,false)
+//     c.stroke()
+
+// }
+
+let x = 100
+let dx = 4
+const radius = 30
+const animate = () => {
+	requestAnimationFrame(animate)
+	c.clearRect(0, 0, innerWidth, innerHeight)
+	c.beginPath()
+	c.arc(x, 300, 30, 0, Math.PI * 2, false)
+	c.strokeStyle = 'green'
+	c.stroke()
+	if (x+radius >innerWidth || x-radius<0) {
+		dx = -dx
+	}
+	x += dx
 }
+
+animate()
